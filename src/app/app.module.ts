ac5c133df2a6fd7f078969c280e776a8d475c9a4
Felipe Colin <leonel.pipe82@gmail.com>
2020-10-29 13:52:22 -0600

    Inicio de la App de Auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
