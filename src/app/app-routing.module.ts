0e8dccb30381805579e662d9fbfb9bb955cbd84d
Felipe Colin <leonel.pipe82@gmail.com>
2020-10-29 14:35:07 -0600

    Se edita app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
+
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'protegida', component: ProtegidaComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
-
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
